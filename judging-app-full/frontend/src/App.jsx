6dfc8f0c68953170fdb0dc862780971957db6716
kshitijjain-art <kshitij.jain@shivalikcollege.edu.in>
2025-12-15 13:13:57 +0530

    Update App.jsx

--- judging-app-full/frontend/src/App.jsx
+++ judging-app-full/frontend/src/App.jsx
@@ -86,7 +86,7 @@ export default function App() {
           setTeamDetails(data);
         }
       });
-  }, [teamId]);
+  }, [name]);
 
   /* ================= LOAD CRITERIA ================= */
 
